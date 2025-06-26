e689cf6b79f765b389f9cdae24ce2f0411ba3001
IH Sajjad <iftekherhossensajjad@gmail.com>
2025-06-26 16:58:03 +0600

    Completed the left side bar menu

---
+++ src/components/left-menu.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import React, { Dispatch, SetStateAction } from "react";
+
+const LeftMenu = ({
+  openMenu,
+  setOpenMenu,
+}: {
+  openMenu: boolean;
+  setOpenMenu: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const menuItems = [
+    "home.png",
+    "all-duas.png",
+    "memorize.png",
+    "bookmark.png",
+    "ruqya.png",
+    "dua.png",
+    "book.png",
+  ];
+  return (
+    <div
+      className={`bg-white md:col-span-1 fixed md:static duration-300 flex flex-col items-center justify-between h-screen py-4 md:py-7 rounded-3xl ${
+        openMenu ? "  left-0 top-0 z-10" : "-left-20 top-0 md:block"
+      }`}
+    >
+      <div className="flex flex-col items-center">
+        <button
+          className={`block md:hidden bg-white rounded-full p-1 ${
+            openMenu ? "-rotate-180" : "hidden"
+          }`}
+          onClick={() => setOpenMenu((p) => !p)}
+        >
+          <Image
+            src={"/icons/arrow-right.png"}
+            alt="Search Icon"
+            width={26}
+            height={26}
+          />
+        </button>
+        <Image src={`/logo.png`} alt="Dua Logo" width={73} height={73} />
+      </div>
+
+      <div className="flex flex-col gap-3 xl:gap-6">
+        {menuItems.map((item) => (
+          <div
+            key={item}
+            className="bg-[var(--primary-bg)] w-[38px] h-[38px] rounded-full flex items-center justify-center"
+          >
+            <Image src={`/icons/${item}`} alt={item} width={20} height={20} />
+          </div>
+        ))}
+      </div>
+
+      <button className="bg-[var(--primary)] h-[56px] w-[56px] flex items-center justify-center rounded-lg">
+        <Image
+          src={`/icons/support.png`}
+          alt="Support icon"
+          width={24}
+          height={24}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default LeftMenu;
--- src/components/main-layout.tsx
+++ src/components/main-layout.tsx
@@ -2,6 +2,7 @@
 
 import { ReactNode, useState } from "react";
 import Navbar from "./navbar";
+import LeftMenu from "./left-menu";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   const [openMenu, setOpenMenu] = useState<boolean>(true);
@@ -9,13 +10,8 @@ const MainLayout = ({ children }: { children: ReactNode }) => {
 
   return (
     <div className="grid md:grid-cols-12 gap-5 p-3 md:p-5">
-      <div
-        className={`bg-purple-500 h-80 md:col-span-1 border fixed md:static duration-300 ${
-          openMenu ? "  left-0 top-[60px] z-10" : "-left-20 top-[60px] md:block"
-        }`}
-      >
-        sidebar
-      </div>
+      <LeftMenu openMenu={openMenu} setOpenMenu={setOpenMenu} />
+
       <div className="col-span-9">
         <Navbar
           openMenu={openMenu}
@@ -25,6 +21,7 @@ const MainLayout = ({ children }: { children: ReactNode }) => {
         />
         {children}
       </div>
+
       <div
         className={`bg-black/20 backdrop-blur-sm h-80 col-span-2 border fixed md:static ${
           openDropdown
@@ -32,7 +29,11 @@ const MainLayout = ({ children }: { children: ReactNode }) => {
             : "-right-20 top-[60px] md:block"
         }`}
       >
-        <div className={`bg-white fixed md:static p-5 ${openDropdown ? "top-[7px] right-2": "hidden md:block"}`}>
+        <div
+          className={`bg-white fixed md:static p-5 ${
+            openDropdown ? "top-[7px] right-2" : "hidden md:block"
+          }`}
+        >
           right Sidebar
         </div>
       </div>
